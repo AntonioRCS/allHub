dbb25f4e1340040e4cbb9b9638c02600a06a835f
Antônio Ricardo <antoniorcs67@gmail.com>
2025-08-21 10:18:00 -0300

    little Square

--- src/pages/LittleSquare/LittleSquare.jsx
+++ src/pages/LittleSquare/LittleSquare.jsx
@@ -11,17 +11,32 @@ const LittleSquare = () => {
 	const [lines] = useState(CreateArray(30));
 	const [col] = useState(CreateArray(35));
 	const [space] = useState(5);
-	const colQnt = 35;
-	let speed = 1;
-	let wallCol = colQnt - 1;
-	let wallPos = [];
-	let score = 0;
+	const [speed] = useState(1);
+	const [score, setScore] = useState(0);
 
 	const movePlayer = (vert, horz) => {
 		const currentField = mainField.current;
-		const playerPos = currentField.querySelector('.cell.player');
-		const playerCord = playerPos.id.split('-');
-		console.log(playerCord)
+
+		let playerCord = currentField.querySelector(".cell.player");
+		playerCord = playerCord.id.split("-");
+		playerCord = playerCord.map((e) => Number(e));
+
+		let futureCord = [playerCord[0] + vert, playerCord[1] + horz];
+		if (futureCord[0] < 0 || futureCord[0] >= lines.length)
+			futureCord[0] = playerCord[0];
+		else if (futureCord[1] < 0 || futureCord[1] >= col.length)
+			futureCord[1] = playerCord[1];
+		currentField.children[playerCord[0]].children[playerCord[1]].className =
+			"cell";
+		if (
+			currentField.children[futureCord[0]].children[futureCord[1]]
+				.className == "cell wall"
+		) {
+			endPlay();
+			return;
+		}
+		currentField.children[futureCord[0]].children[futureCord[1]].className =
+			"cell player";
 	};
 
 	const handleArrowKeyPress = (event) => {
@@ -48,7 +63,9 @@ const LittleSquare = () => {
 		for (let i = 0; i < lines.length; i++) {
 			for (let j = 0; j < col.length; j++) {
 				//querySelector with ID equal 'i-j'
-				currentField.querySelector('[id="'+i+'-'+j+'"]').className = 'cell'
+				currentField.querySelector(
+					'[id="' + i + "-" + j + '"]'
+				).className = "cell";
 			}
 		}
 	};
@@ -57,78 +74,76 @@ const LittleSquare = () => {
 		const currentField = mainField.current;
 		let currentPoint = Math.floor(Math.random() * lines.length);
 		const repeat = lines.length - space;
-		for(let i = 0; i < repeat; i++) {
-			if(currentPoint >= lines.length) currentPoint = 0;
-			currentField.children[currentPoint].children[col.length - 1].className = 'cell wall';
+		for (let i = 0; i < repeat; i++) {
+			if (currentPoint >= lines.length) currentPoint = 0;
+			currentField.children[currentPoint].children[
+				col.length - 1
+			].className = "cell wall";
 			currentPoint++;
 		}
 	};
 
 	const moveWall = () => {
-		let newWallPos = [];
-		if (wallCol == 0) {
-			score = score + 1;
-			document.getElementById("score").innerHTML = `Pontuação: ${score}`;
-			wallPos.forEach((elem) => {
-				const actualCell = document.getElementById(
-					`${elem[0]}-${elem[1]}`
-				);
-				actualCell.className = "cell";
-				createNewWall();
-				return;
+		const currentField = mainField.current;
+		const allWall = currentField.querySelectorAll(".cell.wall");
+		let wallPos = Number(allWall[0].id.split("-")[1]);
+		if (wallPos !== 0) {
+			allWall.forEach((elem) => {
+				const wallId = elem.id.split("-");
+				elem.className = "cell";
+				if (
+					currentField.children[wallId[0]].children[wallPos - 1]
+						.className == "cell player"
+				) {
+					endPlay();
+					return;
+				}
+				currentField.children[wallId[0]].children[
+					wallPos - 1
+				].className = "cell wall";
 			});
 		} else {
-			for (const elem of wallPos) {
-				wallCol = elem[1] - 1;
-				const actualCell = document.getElementById(
-					`${elem[0]}-${elem[1]}`
-				);
-				const futureCell = document.getElementById(
-					`${elem[0]}-${elem[1] - 1}`
-				);
-				if (futureCell.className == "cell player") {
-					endPlay();
-					break;
-				} else {
-					actualCell.className = "cell";
-					futureCell.className = "cell wall";
-					newWallPos.push([elem[0], elem[1] - 1]);
-				}
-			}
-			wallPos = newWallPos;
+			allWall.forEach((elem) => {
+				elem.className = "cell";
+			});
+			createNewWall();
+			setScore((score) => score + 1);
 		}
 	};
 
 	const startPlay = (e) => {
 		const currentField = mainField.current;
 		currentField.focus();
-		score = 0;
+		setScore(0);
 		e.target.disabled = true;
 		currentField.children[0].children[0].className = "cell player";
-		createNewWall()
-		// setIsRunning(true);
+		createNewWall();
+		setIsRunning(true);
 	};
 
 	const endPlay = () => {
-		// setIsRunning(false);
-		startPlayBtn.current.disabled = false
+		const currentField = mainField.current;
 		clearBoard();
+		setIsRunning(false);
+		startPlayBtn.current.disabled = false;
+		currentField.blur();
 	};
 
 	useEffect(() => {
 		let intervalId;
-		if(isRunning) {
-			intervalId = setInterval(moveWall, 25);
+		if (isRunning) {
+			intervalId = setInterval(moveWall, 80);
 		} else {
 			clearInterval(intervalId);
 		}
 		return () => clearInterval(intervalId);
+	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [isRunning]);
 
 	return (
 		<div id="littleSquareBody">
 			<div id="leftField">
-				<button id="startPlay" ref ={startPlayBtn} onClick={startPlay}>
+				<button id="startPlay" ref={startPlayBtn} onClick={startPlay}>
 					Começar
 				</button>
 				<button onClick={endPlay}>Finalizar</button>
